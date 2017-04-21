68c52a29c73bb93d49219b081a47f3f05dba09fc
Amadeu Tolentino <amadeu.tolentino@gmail.com>
2017-04-21 13:03:51 -0400

    Add Screen Boilerplate
    Signed-off-by: Amadeu Tolentino <amadeu.tolentino@gmail.com>

---
+++ screens/AuthScreen.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class AuthScreen extends Component {
+  render(){
+    return (
+      <View>
+        <Text>Hello AuthScreen</Text>
+        <Text>Hello AuthScreen</Text>
+        <Text>Hello AuthScreen</Text>
+        <Text>Hello AuthScreen</Text>
+      </View>
+    )
+  }
+}
+
+export default AuthScreen;
---
+++ screens/DeckScreen.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class DeckScreen extends Component {
+  render(){
+    return (
+      <View>
+        <Text>Hello DeckScreen</Text>
+        <Text>Hello DeckScreen</Text>
+        <Text>Hello DeckScreen</Text>
+        <Text>Hello DeckScreen</Text>
+      </View>
+    )
+  }
+}
+
+export default DeckScreen;
